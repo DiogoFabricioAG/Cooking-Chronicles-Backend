4c14e6be42df2eb654e54d621c36d9171812598b
Diogo <diogo.abregu.g@uni.pe>
2024-03-12 00:03:32 -0500

    new: schemas and routes

--- src/app.py
+++ src/app.py
@@ -1,31 +1,74 @@
-import os
-from flask import Flask,request,jsonify
+from io import BytesIO
+from flask import Flask,request,jsonify,send_file
 from db import db_init
 from ma import ma_init
 from config import Config
-from werkzeug.utils import secure_filename
 from models import *
 from schemas import *
 from flask_migrate import Migrate
 from flask_cors import CORS, cross_origin
-
-
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from PIL import Image
 app = Flask(__name__) 
 app.config.from_object(Config)
 cors = CORS(app,origins=["http://localhost:5173"])
 
 db_init(app)
 ma_init(app)
+
 migrate = Migrate(app, db)
 # ------------------------------------------------- 
 
 ROUTE_USERS = "/api/user/"
+ROUTE_RECIPES = "/api/recipes/"
+# ------------------------------------------------- 
+@app.route('/api/user/<int:user_id>/image', methods=['GET'])
+def get_user_image(user_id):
+    user = UserModel.query.get(user_id)
+    if not user or not user.image:
+        return jsonify({"message": "User or image not found"}), 404
+    image = Image.open(BytesIO(user.image))
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+    img_io = BytesIO()
+    image.save(img_io, 'JPEG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+@app.route(f'{ROUTE_RECIPES}<int:recipe_id>/image', methods=['GET'])
+def get_recipe_image(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    print(recipe)
+    if not recipe or not recipe.image:
+        return jsonify({"message": "User or image not found"}), 404
+    image = Image.open(BytesIO(recipe.image))
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+    img_io = BytesIO()
+    image.save(img_io, 'JPEG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
 @app.route(ROUTE_USERS,methods=["GET"])
 @cross_origin()
 def index():
-    if request.method == "GET":
-        all_users =  UserModel.query.all()
-        return UserSchema(many=True).jsonify(all_users)
+    all_users =  UserModel.query.all()
+    print(all_users)
+    return UserSchema(many=True).jsonify(all_users)
+@app.route(ROUTE_RECIPES,methods=["GET"])
+@cross_origin()
+def recipes():
+    all_recipes =  Recipe.query.all()
+    return RecipeSchema(many=True).dump(all_recipes)
+@app.route(f"{ROUTE_RECIPES}best/<int:id>",methods=["GET"])
+@cross_origin()
+def bestrecipes(id):
+    my_best_recipes =  Recipe.query.filter_by(user_model_id = id)[0:3]
+    return RecipeSchema(many=True).dump(my_best_recipes)
+@app.route(f"{ROUTE_USERS}<int:id>",methods=["GET"])
+@cross_origin()
+def userbyid(id):
+    user = UserModel.query.filter_by(id=id).first()
+    return UserSchema().jsonify(user)
 
 @app.route(f"{ROUTE_USERS}create/",methods=["POST"])
 @cross_origin()
@@ -34,10 +77,32 @@ def createuser():
     db.session.add(user)
     db.session.commit()
     return 'Usuario creado'
+@app.route(f"{ROUTE_RECIPES}create/",methods=["POST"])
+@cross_origin()
+def createrecipe():
+    recipe = Recipe()
+    user = UserModel.query.filter_by(id=int(request.values.get("id_user"))).first()
+
+    recipe.name = request.values.get("name")
+    recipe.description = request.values.get("description")
+    recipe.user_impressions = request.values.get("user_impressions")
+    recipe.image = request.files.get("image").read()
+    recipe.user_model_id = user.id
+    db.session.add(recipe)
+    db.session.commit()
+    return 'Receta creada'
+@app.route(f"{ROUTE_USERS}edit/<int:id>/",methods=["PUT"])
+@cross_origin()
+def edituser(id):
+    user = UserModel.query.filter_by(id=id).first()
+    user.setvalues(request.values)
+    user.image = request.files.get("image").read()
+    db.session.commit()
+    return 'Usuario editado'
 @app.route(f"{ROUTE_USERS}login/",methods=["POST"])
 @cross_origin()
 def loginuser():
-    user = UserModel.query.filter_by(username = request.json["username"]).first()
+    user = UserModel.query.filter_by(username = request.json["username"], password = request.json["password"]).first()
     if user:
         return UserSchema().jsonify(user)
     else:
--- src/models.py
+++ src/models.py
@@ -1,23 +1,50 @@
 from db import db
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(30),unique = True)
+    recipe_id = db.Column(db.Integer,db.ForeignKey("recipe.id"))
+
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), unique = True, nullable = False)
     password = db.Column(db.String(20),  nullable = False)
-    mimetype = db.Column(db.Text, nullable=True)
-    img = db.Column(db.Text, unique=True, nullable=True)
+    image = db.Column(db.LargeBinary,nullable = True)
     description = db.Column(db.Text, nullable = True)
     education = db.Column(db.Text, nullable = True)
     age = db.Column(db.Integer, nullable = True)
     nationality = db.Column(db.Text, nullable = True)
     rank = db.Column(db.Text, nullable = True)
-    speciality = db.Column(db.Text, nullable = True)
+    linkface = db.Column(db.String(100),  nullable = True)
+    linkinsta = db.Column(db.String(100),  nullable = True)
+    linktik = db.Column(db.Text, nullable = True)
     count_recipes = db.Column(db.Integer, default = 0)
     comentrate = db.relationship("Comentrate",backref="comentrates")
     recipe = db.relationship("Recipe",backref="user")
     def __init__(self,username,password) -> None:
         self.username = username
         self.password = password
+    
+    def setvalues(self,data: dict):
+        if data.get("username"):
+            self.username = data.get("username")
+        if data.get("age"):
+            self.age = data.get("age")
+        if data.get("nationality"):
+            self.nationality = data.get("nationality")
+        if data.get("description"):
+            self.description = data.get("description")
+        if data.get("education"):
+            self.education = data.get("education")
+        if data.get("speciality"):
+            self.speciality = data.get("speciality")
+        if data.get("linkinsta"):
+            self.linkinsta = data.get("linkinsta")
+        if data.get("linkface"):
+            self.linkface = data.get("linkface")
+        if data.get("linktik"):
+            self.linktik = data.get("linktik")
+
 
 recipe_ingredient = db.Table("recipe_ingredient",
     db.Column("recipe_id",db.Integer,db.ForeignKey("recipe.id")),
@@ -30,14 +57,14 @@ class Ingredient(db.Model):
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(30),unique = True)
+    image = db.Column(db.LargeBinary,nullable = True)
+    description = db.Column(db.Text, nullable = True)
+    user_impressions = db.Column(db.Text, nullable = True)
+
+    tag = db.relationship("Tags",backref="tags")
     ingredient = db.relationship('Ingredient',secondary = recipe_ingredient, backref = "recipe")
-    tag = db.relationship("Tag",backref="tags")
     user_model_id = db.Column(db.Integer,db.ForeignKey("user_model.id")) 
     comentrate = db.relationship("Comentrate",backref="comentrate")
-class Tags(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(30),unique = True)
-    recipe_id = db.Column(db.Integer,db.ForeignKey("recipe.id"))
 
 class Comentrate(db.Model):
     id = db.Column(db.Integer, primary_key = True)
